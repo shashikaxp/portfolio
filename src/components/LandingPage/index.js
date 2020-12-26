1cd7b7ea06d3fdf2a3030442d65b31d9412be94e
Shashika <shashikaxp@gmail.com>
2020-12-26 20:01:30 +1100

    remove particle js

--- src/components/LandingPage/index.js
+++ src/components/LandingPage/index.js
@@ -1,19 +1,10 @@
 import React from 'react';
-import Background from './Background';
-import DetailsCard from '../DetailsCard';
 
 import './LandingPage.scss';
 import ResponsiveBreakpointHoc from '../hoc/responsiveBreaker';
 
-const Banner = ({ screenSize }) => {
-  return (
-    <div>
-      <Background screenSize={screenSize} />
-      <div className="container">
-        <DetailsCard />
-      </div>
-    </div>
-  );
+const LandingPage = ({ screenSize }) => {
+  return <div>Shashika Weerakkody</div>;
 };
 
-export default ResponsiveBreakpointHoc(Banner);
+export default ResponsiveBreakpointHoc(LandingPage);
