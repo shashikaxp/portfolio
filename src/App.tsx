4d4ea4d6e116d4150c38d0b56529e0ccd1182864
Shashika <shashikaxp@gmail.com>
2021-08-06 23:57:00 +1000

    setup tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return <div>sad</div>;
+  return <div className="border-2 mt-5 text-center text-xl">sad</div>;
 }
 
 export default App;
