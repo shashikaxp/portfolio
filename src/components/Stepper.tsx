16532f06f7bc4bc5571a31f454a76313bdadd6d0
Shashika <shashikaxp@gmail.com>
2021-08-16 09:28:36 +1000

    update experience page

--- src/components/Stepper.tsx
+++ src/components/Stepper.tsx
@@ -2,20 +2,21 @@ import React from 'react';
 
 interface stepperProps {
   steps: string[];
+  changeCompany: (id: string) => void;
 }
 
-export const Stepper: React.FC<stepperProps> = ({ steps }) => {
+export const Stepper: React.FC<stepperProps> = ({ steps, changeCompany }) => {
   return (
     <div className="space-y-5">
       {steps.map((step) => {
         return (
           <div key={step}>
-            <a
-              href="#"
+            <button
+              onClick={() => changeCompany(step)}
               className="w-4 h-4 rounded-full bg-white flex items-center justify-center"
             >
               <div className="w-3 h-3 rounded-full bg-gray-600"></div>
-            </a>
+            </button>
           </div>
         );
       })}
--- src/screens/Experience.tsx
+++ src/screens/Experience.tsx
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import kinzaBg from '../assets/img/kinza_bg.jpeg';
+import epigroBg from '../assets/img/epigro_bg.jpg';
+
+import { find, map } from 'lodash';
 
 import { CompanyProfile } from '../components/CompanyProfile';
 import { Projects } from '../components/Projects';
@@ -7,26 +10,60 @@ import { Stepper } from '../components/Stepper';
 
 interface ExperienceProps {}
 
+interface CompanyData {
+  id: string;
+  name: string;
+  position: string;
+  time: string;
+  description: string;
+  bg: string;
+  projects: string[];
+}
+
 export const Experience: React.FC<ExperienceProps> = ({}) => {
-  const companyData = {
-    name: 'Kinza Pty LTD',
-    position: 'Internship',
-    time: '2015 - 2016 (1 year 2 Months )',
-    description:
-      "It is a long established fact that opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page ed",
+  const companyData: CompanyData[] = [
+    {
+      id: 'kinza',
+      name: 'Kinza Pty LTD',
+      position: 'Internship',
+      time: '2015 - 2016 (1 year 2 Months )',
+      description:
+        "It is a long established fact that opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page ed",
+      bg: kinzaBg,
+      projects: ['1', '2', '3', '4'],
+    },
+    {
+      id: 'epigro',
+      name: 'Epigro Solar Lanka',
+      position: 'Trainee Software Engineer',
+      time: '2015 - 2016 (1 year 2 Months )',
+      description:
+        "It is a long established fact that opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page ed",
+      bg: epigroBg,
+      projects: ['1', '2'],
+    },
+  ];
+
+  const [company, setCompany] = useState(companyData[0]);
+
+  const changeCompany = (id: string) => {
+    const selectedCompany = find(companyData, { id: id });
+    if (selectedCompany) {
+      setCompany(selectedCompany);
+    }
   };
 
   return (
     <div
       className="min-h-screen bg-cover backdrop-blur-md flex items-center justify-center"
-      style={{ backgroundImage: `url(${kinzaBg})` }}
+      style={{ backgroundImage: `url(${company.bg})` }}
     >
-      <Stepper steps={['1', '2', '3', '4']} />
+      <Stepper steps={map(companyData, 'id')} changeCompany={changeCompany} />
       <div className="ml-8 w-1/3">
-        <CompanyProfile {...companyData} />
+        <CompanyProfile {...company} />
       </div>
-      <div className="ml-20">
-        <Projects projects={['1', '2', '3', '4']} />
+      <div className="ml-20 w-2/4">
+        <Projects projects={company.projects} />
       </div>
     </div>
   );
